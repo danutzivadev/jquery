4043b169ce4cf3bbd5caa0290bd9851f60d22802
dnz <danut.ivanescu@outlook.com>
2021-04-06 11:26:01 +0300

    6.04

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-$(".warning").hide().show("slow");
+// Prevent spolierphobes from seeing spoilers
+// Solution : Hide spoilers and reveal them through user interaction
+
+// 1. Hide spoiler
+$(".spoiler span").hide();
+// 2. Add button
+$(".spoiler").append("<button>Reveal Spoiler!</button>");
+// 3. When button pressed
+$(".spoiler button").click(function () {
+    // 3.1 Show spoiler next to the button clicked
+    $(this).prev().show();
+    // 3.2 Get rid of button
+    $(this).remove();
+});
